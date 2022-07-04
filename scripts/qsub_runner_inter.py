5645ae26f703ee743ac9f2d0ff4badd809222cf0
annacuomo <annacuomo922gmail.com>
2022-07-05 09:45:53 +1000

    add estimate betas to runner

--- scripts/qsub_runner_inter.py
+++ scripts/qsub_runner_inter.py
@@ -4,8 +4,9 @@ import pandas as pd
 if __name__ == '__main__':
 
     inputs_folder = "/share/ScratchGeneral/anncuo/OneK1K/input_files_CellRegMap/"
-    fvf_filename = inputs_folder+"fvf_Bcell_eqtls.csv"
-    #fvf_filename = inputs_folder+"fvf_Monocyte_eqtls.csv"
+    # fvf_filename = inputs_folder+"fvf_Bcell_eqtls.csv"
+    # fvf_filename = inputs_folder+"fvf_Monocyte_eqtls.csv"
+    fvf_filename = inputs_folder+"sex_beta_fvf_Monocyte.csv"
     fvf = pd.read_csv(fvf_filename, index_col = 0)
 
     print("prepare job to submit")
@@ -15,13 +16,14 @@ if __name__ == '__main__':
     for j in range(22):
 
         chrom = j+1
-        if chrom not in [1, 2, 6, 7, 11, 12, 17, 19]: continue # ad hoc in case some chromosomes are already finished
+        # if chrom not in [1, 2, 6, 7, 11, 12, 17, 19]: continue # ad hoc in case some chromosomes are already finished
         genes = fvf[fvf['chrom']==int(chrom)]['feature'].unique()
 
         for i in range(len(genes)):
 
-            py = f"/share/ScratchGeneral/anncuo/jupyter/conda_notebooks/envs/cellregmap_notebook/bin/python run_interaction_Bcells_one_gene.py {chrom} {i}"
+            # py = f"/share/ScratchGeneral/anncuo/jupyter/conda_notebooks/envs/cellregmap_notebook/bin/python run_interaction_Bcells_one_gene.py {chrom} {i}"
             # py = f"/share/ScratchGeneral/anncuo/jupyter/conda_notebooks/envs/cellregmap_notebook/bin/python run_interaction_Monocytes_sex_one_gene.py {chrom} {i}"
+            py = f"/share/ScratchGeneral/anncuo/jupyter/conda_notebooks/envs/cellregmap_notebook/bin/python estimate_betas_Monocytes_sex_one_gene.py {chrom} {i}"
             cmd = f"{qsub} \"{py}\" "
             print(cmd)
             os.system(cmd)
