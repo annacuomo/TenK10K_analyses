6cba5df963574627ae17dbaeca327fadf06f126d
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-08-03 17:36:11 +1200

    add R color brewer library

--- scripts/plotting_scripts/plot_betaGxC.R
+++ scripts/plotting_scripts/plot_betaGxC.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
 library(Seurat)
+library(RColorBrewer)
 
 sceb_filename = "/share/ScratchGeneral/anncuo/OneK1K/b_cells_phate_slingshot.RDS"
 sceb = readRDS(sceb_filename)
