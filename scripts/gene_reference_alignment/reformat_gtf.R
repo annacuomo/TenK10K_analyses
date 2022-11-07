3a6d996263f5ba6a9a6f74b8380e1eb62e15a045
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-11-08 10:06:36 +1100

    add transcripts and split tags

--- scripts/gene_reference_alignment/reformat_gtf.R
+++ scripts/gene_reference_alignment/reformat_gtf.R
@@ -2,9 +2,10 @@
 library(plyr)
 
 # Get object (Garvan HPC cluster)
+# downloaded from https://www.gencodegenes.org/human/release_{}.html
 gene_ref_folder <- "/share/ScratchGeneral/anncuo/OneK1K/gene_ref/"
-# gtf_file <- paste0(gene_ref_folder, "gencode.v42.basic.annotation.gtf")
-gtf_file <- paste0(gene_ref_folder, "gencode.v42.chr_patch_hapl_scaff.annotation.gtf")
+gtf_file <- paste0(gene_ref_folder, "gencode.v42.basic.annotation.gtf")  # v42
+gtf_file <- paste0(gene_ref_folder, "gencode.v38.annotation.gtf")        # v38
 
 # Load object (Gencode v42)
 gtf <- read.table(gtf_file, header = FALSE, sep = "\t")
@@ -48,6 +49,47 @@ full_df$havana_gene <- gsub(";", "", full_df$havana_gene)
 csv_filename <- gsub(".gtf", ".genesonly.csv", gtf_file)
 write.csv(full_df, csv_filename)
 
+
+# Select transcripts only
+gtf_t_df <- gtf_df[gtf_df$V3 == "transcript", ]
+print(paste0("Total number of transcripts: ", nrow(gtf_t_df)))
+
+# Rename columns
+colnames(gtf_t_df) <- c("seqname", "source", "feature",
+    "start", "end", "score", "strand", "frame", "attribute")
+
+# Extract attributes
+attributes_df <- data.frame()
+for (i in seq_len(nrow(gtf_t_df))){
+    line <- gtf_t_df$attribute[i]
+    elems <- unlist(strsplit(line, "; "))
+    mat <- matrix(unlist(strsplit(elems, " ")), nrow = 2, ncol = length(elems))
+    df_curr <- as.data.frame(mat)
+    colnames(df_curr) <- mat[1, ]
+    n <- length(colnames(df_curr)[grep("tag", colnames(df_curr))]) # split tags
+    if (n > 1) {
+        colnames(df_curr)[grep("tag", colnames(df_curr))] <- paste0("tag", 1:n)
+        }
+    df_curr <- df_curr[-1, ]
+    attributes_df <- rbind.fill(attributes_df, df_curr)
+}
+
+# Add reformatted attributes to data frame
+full_df <- cbind(gtf_t_df, attributes_df)
+full_df$attribute <- c()
+
+# Add columns Ensembl Gene ID (no version)
+full_df$ensembl_gene_id <- gsub("\\..*", "", full_df$gene_id)
+
+# Clean attribute columns
+full_df$tag1 <- gsub(";", "", full_df$tag1)
+full_df$tag2 <- gsub(";", "", full_df$tag2)
+full_df$level <- gsub(";", "", full_df$level)
+full_df$havana_gene <- gsub(";", "", full_df$havana_gene)
+
+# Save formatted object
+csv_filename <- gsub(".gtf", ".transcriptsonly.csv", gtf_file)
+write.csv(full_df, csv_filename)
 
 
 
