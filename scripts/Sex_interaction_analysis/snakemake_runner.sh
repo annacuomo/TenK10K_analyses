d9f2fc5c609117fd7a0c1f4e879900cf90e3e31e
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2023-02-02 13:43:38 +1100

    add V for env and no hup

--- scripts/Sex_interaction_analysis/snakemake_runner.sh
+++ scripts/Sex_interaction_analysis/snakemake_runner.sh
@@ -2,18 +2,21 @@ SNAKEFILE="./snakemake_sex_interaction_singlecells.smk"
 CONFIG="cluster.json"
 LOG="/share/ScratchGeneral/anncuo/OneK1K/Sex_interactions/Monocytes/logs"
 
-snakemake \
-  --snakefile $SNAKEFILE \
-  --keep-going \
-  --rerun-incomplete \
-  --jobs 20 \
-  --cluster \
-    "qsub -S /bin/bash \
-    -q short.q \
-    -r yes \
-    -pe smp 20 \  # number of threads
-    -l tmp_requested=20G \
-    -l mem_requested=20G \
-    -o $LOG" \
-    -j y \  # this is to join error and log
+nohup \
+  snakemake \
+    --snakefile $SNAKEFILE \
+    --keep-going \
+    --rerun-incomplete \
+    --printshellcmds \
+    --jobs 20 \
+    --cluster \
+      "qsub -S /bin/bash \
+      -q short.q \
+      -r yes \
+      -pe smp 20 \  # number of threads
+      -l tmp_requested=20G \
+      -l mem_requested=20G \
+      -o $LOG \
+      -j y \  # this is to join error and log
+      -V"
   > $LOG/snake_`date +%Y-%m-%d.%H:%M:%S`.log 
